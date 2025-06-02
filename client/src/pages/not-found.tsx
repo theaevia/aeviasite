8208e3b751bd30b8bd75b5de2a7f5dbae8d58f1f
teeokh <doublet123t@gmail.com>
2025-06-02 20:38:48 +0100

    Fix not found page

--- client/src/pages/not-found.tsx
+++ client/src/pages/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
           </div>
 
           <p className="mt-4 text-sm text-gray-600">
-            Did you forget to add the page to the router?
+            Did you take a wrong turn?
           </p>
         </CardContent>
       </Card>
