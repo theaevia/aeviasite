cf987f89993490487a08098b37bab6165b45a1c0
Tee <139244119+teeokh@users.noreply.github.com>
2025-06-10 21:17:47 +0100

    Fix map embed details

--- client/src/components/LocationMap.tsx
+++ client/src/components/LocationMap.tsx
@@ -5,11 +5,10 @@ export default function LocationMap() {
     <div className="w-full h-96">
       <iframe
         title="The Aevia Location"
-        src="https://maps.google.com/maps?q=51.5310793,-0.1203023&amp;z=17&amp;output=embed"
-        width="100%"
-        height="100%"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d198.41522661709957!2d-0.1203023!3d51.5310793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761be0d6318279:0x6231887d02d12d6b!2sThe%20Aevia!5e0!3m2!1sen!2suk!4v1719577257843!5m2!1sen!2suk"
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
+        allowFullScreen
         className="w-full h-full border-0 rounded-2xl"
       ></iframe>
     </div>
