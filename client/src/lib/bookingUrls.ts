9c3c9c9d32a00461673cfd0add4166842faaaa48
teeokh <doublet123t@gmail.com>
2025-09-21 23:31:24 +0100

    consultation link cleanups

--- client/src/lib/bookingUrls.ts
+++ client/src/lib/bookingUrls.ts
@@ -1,2 +1,2 @@
-export const SKIN_CONSULTATION_URL = 'https://app.squareup.com/appointments/buyer/widget/flwwunfdy1hm72/L1TKSRMBS3N9H';
+export const SKIN_CONSULTATION_URL = 'https://book.squareup.com/appointments/flwwunfdy1hm72/location/L1TKSRMBS3N9H/services';
 export const MIND_DISCOVERY_URL = 'https://book.squareup.com/appointments/sveent13urj63l/location/L1TKSRMBS3N9H/services';
