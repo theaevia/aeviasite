46fa86f510fe462dab32b5c694d314963fc5e674
teeokh <doublet123t@gmail.com>
2025-09-21 18:49:07 +0100

    author slug changes

--- apps/journal/.astro/content-modules.mjs
+++ apps/journal/.astro/content-modules.mjs
@@ -1,7 +1,4 @@
 
 export default new Map([
-["src/content/posts/testing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftesting.mdx&astroContentModuleFlag=true")],
-["src/content/posts/longevity.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Flongevity.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post.mdx&astroContentModuleFlag=true")],
-["src/content/posts/the-aevia-mind-protocol.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fthe-aevia-mind-protocol.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest.mdx&astroContentModuleFlag=true")]]);
 		
