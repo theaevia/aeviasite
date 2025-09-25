5bfd4e31ef6961d67061cb265bbc7443119ba2a0
teeokh <doublet123t@gmail.com>
2025-09-25 20:11:48 +0100

    Sitemaps to public folder

--- apps/journal/.astro/content-modules.mjs
+++ apps/journal/.astro/content-modules.mjs
@@ -1,5 +1,1 @@
-
-export default new Map([
-["src/content/posts/another-test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fanother-test.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post.mdx&astroContentModuleFlag=true")]]);
-		
+export default new Map();
